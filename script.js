c660295e9ad3437860effa6e073b8b1411ee331f
FinestAF <30800403+FinestAF@users.noreply.github.com>
2025-04-10 23:35:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-let progress = 10;
-document.addEventListener("click", function () {
-  progress += 10;
-  if (progress > 100) progress = 100;
-  document.getElementById("progress-bar").style.width = progress + "%";
-});
